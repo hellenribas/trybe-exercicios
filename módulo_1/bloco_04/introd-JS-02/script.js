6656f7bfaf3a39cf3541d36cdaffc6d903841c11
hellenribas <tchwtrabalho@gmail.com>
2021-11-26 16:00:02 -0300

    exercicio 2

---
+++ módulo_1/bloco_04/introd-JS-02/script.js
@@ -0,0 +1,47 @@
+// let movie = "Avengers"; //string literal
+// let score = 10; //number literal
+// let isValid = true; //boolean 
+// let name; //undefined
+// let color = null; //redifinição de valor
+
+//É possível usar operadores matemáticos
+
+let salary = 10;
+
+// console.log(salary + salary); resultado 20
+// console.log(salary * salary); resultado 100
+// console.log(salary - salary); resultado 0
+// console.log(salary / salary); resultado 1
+// console.log(salary ** salary); resultado 10000000000
+
+// salary++;
+// console.log(salary); resultado foi 11
+
+// salary--;
+// console.log(salary); resultado foi 9
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(base);
+console.log(height);
+console.log(area);
+
+const perimeter = base + height;
+console.log(perimeter);
+
