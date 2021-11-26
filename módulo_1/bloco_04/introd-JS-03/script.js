2b048b8f1a27b77378d37a06f31e106deeb799f7
hellenribas <tchwtrabalho@gmail.com>
2021-11-26 16:15:42 -0300

    exercicio if/else

---
+++ módulo_1/bloco_04/introd-JS-03/script.js
@@ -0,0 +1,20 @@
+// // if(condição) {
+//     //codigo
+// }
+// else if (outracondicao) {
+//     outracondicao
+// }
+// else {
+//     condicao
+// }
+
+const nota = 40;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota < 80 && nota > 60) {
+    console.log("Você está na nossa lista de espera");
+} else {
+    console.log("VocÊ foi reprovado(a)")
+}
+
