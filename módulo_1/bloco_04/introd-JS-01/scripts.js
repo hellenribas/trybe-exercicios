691dfaf776fc0df047bd532343088d8000c22138
hellenribas <tchwtrabalho@gmail.com>
2021-11-26 15:34:59 -0300

    primeiro exercicio variaveis

---
+++ módulo_1/bloco_04/introd-JS-01/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Hellen";
+const birthCity = "Paracambi";
+let birthYear = 2002;
+birthYear = 2030;
+birthCity = "Rio de Janeiro"; //Dá erro pois ao declarar uma constante, seu valor não pode ser alterado//
+
+console.log(myName, birthCity, birthYear);
+
