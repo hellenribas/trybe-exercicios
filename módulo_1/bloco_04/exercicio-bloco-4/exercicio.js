e262443b6f2135ffcd78248165af7f39d27adc49
hellenribas <tchwtrabalho@gmail.com>
2021-11-30 11:51:03 -0300

    exercicio ate o 9

---
+++ módulo_1/bloco_04/exercicio-bloco-4/exercicio.js
@@ -0,0 +1,114 @@
+// //Operação básica
+// let a = 10;
+// let b = 40;
+// let soma = a + b;
+// console.log(soma);
+// let subtracao = a - b;
+// console.log(subtracao);
+// let multiplicacao = a * b;
+// console.log(multiplicacao);
+// let divisao = a / b;
+// console.log(divisao);
+// let modulo = a % b;
+// console.log(modulo);
+
+// //comparacao
+// let num1 = 5;
+// let num2 = 6;
+// if (num1 > num2) {
+//     console.log(num1);
+// } else {
+//     console.log(num2);
+// }
+
+// //comparacao 3 números 
+// let number1 = 50;
+// let number2 = 68;
+// let number3 = 90;
+// if (number1 > number2 && number1 > number3) {
+//     console.log(number1);
+// } else if (number2 > number1 && number2 > number3) {
+//     console.log(number2);
+// } else if (number3 > number1 && number3 > number2) {
+//     console.log(number3);
+// }
+
+// //positive ou negative
+
+// let numero = -5;
+// if (numero > 0){
+// console.log('positive');
+// } else {
+//     console.log('negative');
+// }
+
+// //ângulos internos de um triângulo
+// let ang1 = 10;
+// let ang2 = 120;
+// let ang3 = 200;
+
+// if (ang1 + ang2 + ang3 === 180) {
+//     console.log('é um triângulo (true)');
+// } else if (ang1 + ang2 + ang3 < 180 || ang1 + ang2 + ang3 > 180 ) {
+//     console.log('Não é um triÂngulo (false)');
+// } else {
+//     console.log('erro');
+// }
+
+// //Xadrez
+// let text = 'RAINHA';
+// let peca = text.toLowerCase();
+
+// if (peca === 'piao') {
+//       console.log('movimento para frente');
+// } else if (peca === 'torre') {
+//     console.log('movimento para frente e para trás')
+// } else if (peca === 'bispo') {
+//     console.log('movimento para diagonal')
+// } else if (peca === 'cavalo') {
+//     console.log('movimento em L')
+// } else if (peca === 'rainha') {
+//     console.log('movimento para todos os lados')
+// } else if (peca === 'rei') {
+//     console.log('movimento em uma casa')
+// }
+
+
+//PORCENTAGEM
+//TENTAR MAIS TARDE
+// let nota = -10;
+// if (nota >= 90 && nota <= 100 ) {
+//     console.log('nota: A');
+// } else if (nota >= 80) {
+//     console.log('nota: B');
+// } else if (nota >= 70) {
+//     console.log('nota: C');
+// } else if (nota >= 60) {
+//     console.log('nota: D');
+// } else if (nota >= 50) {
+//     console.log('nota: E');
+// } else if (nota < 50 && nota >= 0); {
+//     console.log('nota: F');
+// } 
+
+//TRUE || FALSE
+
+// const num1 = 5;
+// const num2 = 7;
+// const num3 = 9;
+
+// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+// const num1 = 6;
+// const num2 = 8;
+// const num3 = 10;
+
+// if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
